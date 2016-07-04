4ff815e219da2377d1e6f7e3c97337c95e63454f
Vic <4944995@gmail.com>
2016-07-04 20:29:44 +0300

    04/07/16 evening

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ $(document).ready(function(){
 		$('body,html').animate({scrollTop: top}, 1000);
 	});
 });
+$(document).ready(function(){
+	$("#upbtn").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top-65;
+		
+		//анимируем переход на расстояние - top за 1000 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
