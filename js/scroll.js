ba4c8bceb4bfcca3234ae2f29e6e779d7d50d492
Vic <4944995@gmail.com>
2016-07-05 22:02:00 +0300

    05/07/16 evening

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(document).ready(function() {
+     $(window).scroll(function() {
+         if($(this).scrollTop() != 0) {
+             $('#goToUp').fadeIn();
+         } else {
+             $('#goToUp').fadeOut();
+         }
+     });
+     $('#goToUp').click(function () {
+         $('body,html').animate({scrollTop:0},800);
+     });
+ });
+ 
